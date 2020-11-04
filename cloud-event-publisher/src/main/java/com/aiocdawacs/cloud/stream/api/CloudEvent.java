4a93ab1b8c7dd3e096854bebd690593f3d2d009d
girishaiocdawacs <girish.mahajan@aiocdawacs.com>
2020-11-04 13:55:21 +0530

    Initial checkin

---
+++ cloud-event-publisher/src/main/java/com/aiocdawacs/cloud/stream/api/CloudEvent.java
@@ -0,0 +1,16 @@
+package com.aiocdawacs.cloud.stream.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CloudEvent {
+
+    private int id;
+    private String name;
+    private String source;
+    private String type;
+}
---
+++ cloud-event-publisher/src/test/java/com/aiocdawacs/cloud/stream/api/SpringCloudStreamPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiocdawacs.cloud.stream.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
