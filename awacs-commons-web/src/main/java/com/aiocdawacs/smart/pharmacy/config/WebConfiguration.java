6e7d181fb34c47e7826b3716e87d6b4058123860
girishaiocdawacs <girish.mahajan@aiocdawacs.com>
2021-03-09 16:15:50 +0530

    awacsdev platform added

--- awacs-commons-web/src/main/java/com/aiocdawacs/smart/pharmacy/config/WebConfiguration.java
+++ awacs-commons-web/src/main/java/com/aiocdawacs/smart/pharmacy/config/WebConfiguration.java
@@ -1,6 +1,8 @@
 package com.aiocdawacs.smart.pharmacy.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +12,16 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/", "/actuator/info");
+    }
+    
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(Boolean.TRUE);
+        loggingFilter.setIncludeQueryString(Boolean.TRUE);
+        loggingFilter.setIncludePayload(Boolean.TRUE);
+        loggingFilter.setIncludeHeaders(Boolean.TRUE);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
     }
 }
